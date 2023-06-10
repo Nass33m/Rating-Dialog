397ffb5d8b7b255634ec78415cdf9c5199ef5ab8
Nass33m <130575772+Nass33m@users.noreply.github.com>
2023-06-10 12:09:16 +0100

    Create Utils.java

---
+++ rating-diaog/src/main/java/com/nas33m/materialrating/Utils.java
@@ -0,0 +1,19 @@
+package com.nas33m.materialrating;
+
+import android.util.Log;
+
+public class Utils {
+
+    public static final boolean IS_TRIAL = false;
+
+    public static void getErrors(final Exception e) {
+        final String stackTrace = "Vimal ::" + Log.getStackTraceString(e);
+        Utils.sout("" + stackTrace);
+    }
+
+    public static void sout(final String msg) {
+        if (IS_TRIAL) {
+            System.out.println("Vimal :: " + msg);
+        }
+    }
+}
